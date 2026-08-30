388c0994b7721a1614afb05088401c047d271583
Kinzang Tobden <07240013.sherubtse@rub.edu.bt>
2026-08-30 11:04:49 +0600

    Unit III: MVC scaffolding, partials, about page, restaurants, menu, and ordering

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,12 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/homeController');
 const aboutController = require('../controllers/aboutController');
+const menuController = require('../controllers/menuController');
+const orderController = require('../controllers/orderController');
 
 router.get('/', homeController.getHome);
 router.get('/about', aboutController.getAbout);
-
-module.exports = router;
-
-const menuController = require('../controllers/menuController');
-
 router.get('/menu', menuController.getMenu);
-
-const orderController = require('../controllers/orderController');
-
 router.post('/orders', orderController.createOrder);
 
+module.exports = router;
